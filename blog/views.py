f1e78b73e35ad2f8534bd045c9e507656a393214
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-10-18 20:33:38 +0100

    Add paginator and create comments and share form

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,66 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+# def post_list(request):
+    # post_list = Post.published.all()
+    # paginator = Paginator(post_list, 3)
+    # page_number = request.GET.get('page', 1)
+    # try:
+    #     posts = paginator.page(page_number)
+    # except PageNotAnInteger:
+    #     posts = paginator.page(1)
+    # except EmptyPage:
+    #     posts = paginator.page(paginator.num_pages)
+    # return render(
+    #     request,
+    #     'blog/post/list.html',
+    #     {'posts': posts}
+    # )
 
-def post_detail(request, id):
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
-        Post, id=id, 
-        status=Post.Status.PUBLISHED)
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
     return render(
         request,
         'blog/post/detail.html',
         {'post': post}
+    )
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+        'post': post,
+        'form': form}
     )
