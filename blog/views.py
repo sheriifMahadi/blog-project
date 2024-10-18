df85b06b7454c8d78ee28c786074884787b9f336
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-10-18 22:54:26 +0100

    create comment models

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView
+from django.core.mail import send_mail
+
 from .models import Post
 from .forms import EmailPostForm
 
@@ -49,12 +51,31 @@ def post_share(request, post_id):
         status=Post.Status.PUBLISHED
     )
 
+    sent = False 
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
     else:
         form = EmailPostForm()
     return render(
@@ -62,5 +83,7 @@ def post_share(request, post_id):
         'blog/post/share.html',
         {
         'post': post,
-        'form': form}
+        'form': form,
+        'sent': sent
+        }
     )
