d0da043380c5718687a187722567c6ed81f0f5ef
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-10-26 19:53:07 +0100

    add a search engine to app

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,14 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank)
 from django.db.models import Count
 from taggit.models import Tag
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
@@ -135,3 +139,31 @@ def post_comment(request, post_id):
             }
         )
     
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
+                ).filter(rank__gte=0.3)
+                .order_by('-rank')
+            )
+            print(results)
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+        'form': form,
+        'query': query,
+        'results': results
+        }
+    )
